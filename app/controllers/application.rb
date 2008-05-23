6373d6dcc63b8ad50744ce3831de3a0ab33013b0
Kenneth Kalmer <kenneth@clearplanet.co.za>
2008-05-23 15:51:22 +0200

    Skeleton Rails 2.0.2 app for bind-dlz-on-rails

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'dc25a2ea6f551d9218bcb5f35625ed5e'
+end
