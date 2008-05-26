f55ed4b00311dff9bde61128d5907a1a920afae7
Kenneth Kalmer <kenneth@clearplanet.co.za>
2008-05-26 18:20:41 +0200

    Added mocha for use with rspec, added 'vendor everything' configuration

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,9 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
+  config.load_paths += Dir["#{RAILS_ROOT}/vendor/gems/**"].map do |dir| 
+    File.directory?(lib = "#{dir}/lib") ? lib : dir
+  end
 
   # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)
