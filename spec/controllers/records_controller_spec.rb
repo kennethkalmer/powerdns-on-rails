4bb5fb217bbb05be81a06e1299f840de4a0b4b76
keegan <keegan@inx.co.za>
2008-07-04 15:53:16 +0200

    Spec'ing the records controller.

--- spec/controllers/records_controller_spec.rb
+++ spec/controllers/records_controller_spec.rb
@@ -1,10 +1,66 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe RecordsController do
-  fixtures(:all)
+  fixtures :users, :zones
+  
+  before( :each ) do
+    session[:user_id] = users( :admin ).id
+    @zone = zones( :example_com )
+  end
   
   it "should create a new record when valid" do
-    post :create, :record => { }
+    Zone.stubs( :find ).with( @zone.id.to_s, :user => users( :admin ) ).returns( @zone )
+    
+    record = Record.new
+    
+    params = {  
+      'host' => "@", 
+      'ttl' => "864400", 
+      'type' => "NS", 
+      'data' => "n3.example.com"
+    }
+    @zone.records.expects( :new ).with( params ).returns( record )
+    
+    record.expects( :zone_id= ).with( @zone.id )
+    record.expects( :save ).returns( true )
+    
+    post :create, :zone_id => @zone.id, :record => params
+    
+    assigns[:zone].should_not be_nil
+    assigns[:record].should_not be_nil
+    response.should be_redirect
+    response.should redirect_to( zone_path( assigns[:zone] ) )
+  end
+  
+  it "shouldn't save an invalid record" do
+    Zone.stubs( :find ).with( @zone.id.to_s, :user => users( :admin ) ).returns( @zone )
+    
+    record = Record.new
+    
+    params = {  
+      'host' => "@", 
+      'ttl' => "864400", 
+      'type' => "NS", 
+      'data' => "n3.example.com"
+    }
+    
+    @zone.records.expects( :new ).with( params ).returns( record )
+    record.expects( :zone_id= ).with( @zone.id )
+    record.expects( :save ).returns( false )
+    
+    post :create, :zone_id => @zone.id, :record => params
+  end
+  
+  it "should update a valid record" do
+    pending
+  end
+  
+  it "shouldn't update an invalid record" do
+    pending
+  end
+  
+  it "should destroy a record when requested to do so" do
+    pending
   end
 end
 
