78096cb822ff7e90114d26d8aa777afe86c3a5ec
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2008-07-27 19:55:18 +0200

    Turned off production email errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,4 +16,4 @@ config.action_view.cache_template_loading            = true
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
 # Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = false
