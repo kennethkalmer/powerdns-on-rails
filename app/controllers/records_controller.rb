f6b163c1bca8adf3f0f00ced6f10bb3386ac7cfe
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2008-09-29 20:16:41 +0200

    Added initial token support to records controller

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,14 +1,20 @@
 class RecordsController < ApplicationController
   
-  require_role [ "admin", "owner" ]
+  require_role [ "admin", "owner" ], :unless => "token_user?"
   
   before_filter :get_zone
+  before_filter :restrict_token_movements, :except => [:create, :update, :destroy]
   
   def new
     @record = @domain.records.new
   end
   
   def create
+    if current_token && !current_token.allow_new_records?
+      render :text => 'Token not authorized', :status => 403
+      return
+    end
+    
     @record = @domain.send( "#{params[:record][:type].downcase}_records".to_sym ).new( params[:record] )
     if @record.save
       flash.now[:info] = "Record created!"
@@ -24,6 +30,12 @@ class RecordsController < ApplicationController
   
   def update
     @record = @domain.records.find( params[:id] )
+    
+    if current_token && !current_token.can_change?( @record )
+      render :text => 'Token not authorized', :status => 403
+      return
+    end
+    
     if @record.update_attributes( params[:record] )
       flash.now[:info] = "Record udpated!"
     else
@@ -34,6 +46,13 @@ class RecordsController < ApplicationController
   
   def destroy
     @record = @domain.records.find( params[:id] )
+    
+    if current_token && !current_token.remove_records? && 
+        !current_token.can_change?( @record )
+      render :text => 'Token not authorized', :status => 403
+      return
+    end
+    
     @record.destroy
     redirect_to domain_path( @domain )
   end
@@ -48,9 +67,21 @@ class RecordsController < ApplicationController
     end
   end
   
-  protected
+  private
   
   def get_zone
     @domain = Domain.find(params[:domain_id], :user => current_user)
+    
+    if current_token && @domain != current_token.domain
+      render :text => 'Token not authorized', :status => 403
+      return false
+    end
+  end
+  
+  def restrict_token_movements
+    return unless current_token
+    
+    render :text => 'Token not authorized', :status => 403
+    return false
   end
 end
--- spec/controllers/records_controller_spec.rb
+++ spec/controllers/records_controller_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe RecordsController, "and non-SOA records" do
+describe RecordsController, ", users, and non-SOA records" do
   fixtures :users, :domains, :records
   
   before( :each ) do
@@ -79,7 +79,7 @@ describe RecordsController, "and non-SOA records" do
   end
 end
 
-describe RecordsController, "and SOA records" do
+describe RecordsController, ", users, and SOA records" do
   fixtures :all
   
   it "should update when valid" do
@@ -95,5 +95,188 @@ describe RecordsController, "and SOA records" do
     
     target_soa.reload
     target_soa.contact.should eql('dnsadmin@example.com')
+  end
+end
+
+describe RecordsController, "and tokens" do
+  fixtures :auth_tokens, :domains, :records
+  
+  before( :each ) do
+    @domain = domains( :example_com )
+    @token = AuthToken.new( :domain => @domain )
+  end
+  
+  it "should not be allowed to touch the SOA record" do
+    tokenize_as(:token_example_com)
+    
+    target_soa = records(:example_com_soa)
+    
+    lambda {
+      put "update_soa", :id => target_soa.id, :domain_id => target_soa.domain.id,
+        :soa => { 
+          :primary_ns => 'ns1.example.com', :contact => 'dnsadmin@example.com',
+          :refresh => "10800", :retry => "10800", :minimum => "10800", :expire => "604800"
+        }
+        target_soa.reload
+    }.should_not change( target_soa, :contact )
+  end
+  
+  it "should not allow new NS records" do
+    controller.stubs(:current_token).returns(@token)
+    
+    params = {
+      'name' => '',
+      'ttl' => '86400',
+      'type' => 'NS',
+      'content' => 'n3.example.com'
+    }
+    
+    lambda {
+      post :create, :domain_id => @domain.id, :record => params
+    }.should_not change( @domain.records, :size )
+    
+    response.should_not be_success
+    response.code.should == "403"
+  end
+  
+  it "should not allow updating NS records" do
+    controller.stubs(:current_token).returns(@token)
+    
+    record = records(:example_com_ns_ns1)
+    
+    params = {
+      'name' => '',
+      'ttl' => '86400',
+      'type' => 'NS',
+      'content' => 'ns1.somewhereelse.com'
+    }
+    
+    lambda {
+      put :update, :id => record.id, :domain_id => @domain.id, :record => params
+      record.reload
+    }.should_not change( record, :content )
+    
+    response.should_not be_success
+    response.code.should == "403"
+  end
+  
+  it "should create when allowed" do
+    @token.allow_new_records = true
+    controller.stubs(:current_token).returns(@token)
+    
+    params = {
+      'name' => 'test',
+      'ttl' => '86400',
+      'type' => 'A',
+      'content' => '127.0.0.2'
+    }
+    
+    lambda {
+      post :create, :domain_id => @domain.id, :record => params
+    }.should change( @domain.records, :size )
+    
+    response.should be_success
+    
+    assigns[:domain].should_not be_nil
+    assigns[:record].should_not be_nil
+  end
+  
+  it "should not create if not allowed" do
+    controller.stubs(:current_token).returns(@token)
+    
+    params = {  
+      'name' => "test", 
+      'ttl' => "864400", 
+      'type' => "A", 
+      'content' => "127.0.0.2"
+    }
+    
+    lambda {
+      post :create, :domain_id => @domain.id, :record => params
+    }.should_not change( @domain.records, :size )
+    
+    response.should_not be_success
+    response.code.should == "403"
+  end
+  
+  it "should update when allowed" do
+    record = records(:example_com_a_www)
+    @token.can_change( record )
+    controller.stubs(:current_token).returns( @token )
+    
+    params = {  
+      'name' => "www", 
+      'ttl' => "864400", 
+      'type' => "A", 
+      'content' => "10.0.1.10"
+    }
+    
+    lambda {
+      put :update, :id => record.id, :domain_id => @domain.id, :record => params
+      record.reload
+    }.should change( record, :content )
+    
+    response.should be_success
+    response.should render_template("update")
+  end
+  
+  it "should not update if not allowed" do
+    record = records(:example_com_a_www)
+    controller.stubs(:current_token).returns(@token)
+    
+    params = {  
+      'name' => "www", 
+      'ttl' => '',
+      'type' => "A", 
+      'content' => "10.0.1.10"
+    }
+    
+    lambda {
+      put :update, :id => record.id, :domain_id => @domain.id, :record => params
+      record.reload
+    }.should_not change( record, :content )
+    
+    response.should_not be_success
+    response.code.should == "403"
+  end
+  
+  it "should destroy when allowed" do
+    record = records(:example_com_mx)
+    @token.can_change( record )
+    @token.remove_records=( true )
+    controller.stubs(:current_token).returns(@token)
+    
+    lambda {
+      delete :destroy, :domain_id => @domain.id, :id => record.id
+    }.should change( @domain.records, :size ).by(-1)
+    
+    response.should be_redirect
+    response.should redirect_to( domain_path( @domain ) )
+  end
+  
+  it "should not destroy records if not allowed" do
+    controller.stubs(:current_token).returns( @token )
+    
+    lambda {
+      delete :destroy, :domain_id => @domain.id, :id => records(:example_com_a)
+    }.should_not change( @domain.records, :count )
+    
+    response.should_not be_success
+    response.code.should == "403"
+  end
+  
+  it "should not allow tampering with other domains" do
+    @token.allow_new_records=( true )
+    controller.stubs( :current_token ).returns( @token )
+    
+    record = {
+      'name' => 'evil',
+      'type' => 'A',
+      'content' => '127.0.0.3'
+    }
+    
+    post :create, :domain_id => domains(:example_net).id, :record => record
+    
+    response.code.should == "403"
   end
 end
