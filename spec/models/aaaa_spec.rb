2991168efd20638aef7864ad21703af8a8a3043d
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2008-10-22 17:16:36 +0200

    Skeleton improvements to AAAA spec

--- spec/models/aaaa_spec.rb
+++ spec/models/aaaa_spec.rb
@@ -1,9 +1,22 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe AAAA do
+describe AAAA, "when new" do
   
-  it "should have tests" do
-    violated "Write missing specs!"
+  before(:each) do
+    @aaaa = AAAA.new
+  end
+  
+  it "should be invalid by default" do
+    @aaaa.should_not be_valid
+  end
+  
+  it "should only accept IPv6 address as content" do
+    violated "Figure this out"
+  end
+  
+  it "should not act as a CNAME" do
+    @aaaa.content = 'google.com'
+    @aaaa.should have(1).error_on(:content)
   end
   
 end
