c740b5e4e11df2d9b708320cb39257c1532ae4d8
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2008-12-27 23:15:56 +0200

    More upgrade cleanups

--- config/environment.rb
+++ config/environment.rb
@@ -39,9 +39,6 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
-  config.load_paths += Dir["#{RAILS_ROOT}/vendor/gems/**"].map do |dir| 
-    File.directory?(lib = "#{dir}/lib") ? lib : dir
-  end
 
   # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)
@@ -74,3 +71,4 @@ Rails::Initializer.run do |config|
   # Make Active Record use UTC-base instead of local time
   # config.active_record.default_timezone = :utc
 end
+
