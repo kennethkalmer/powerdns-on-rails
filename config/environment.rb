6e8932419dbdda9ba59322756460b58553e552bb
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2009-05-14 23:29:29 +0200

    HAML 2.0.9

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,9 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem 'haml', :version => '2.0.9'
+  config.gem 'mocha', :version => '0.9.3'
+  config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
