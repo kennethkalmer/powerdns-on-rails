dd5788141b162b7cc6b282abd88055d209f3db20
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2009-06-18 23:09:16 +0200

    Upgraded mocha and factory_girl

--- config/environment.rb
+++ config/environment.rb
@@ -20,8 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'haml', :version => '2.0.9'
-  config.gem 'mocha', :version => '0.9.3'
-  config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
+  config.gem 'mocha', :version => '0.9.5'
+  config.gem 'thoughtbot-factory_girl', :version => '1.2.1', :lib => 'factory_girl', :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
