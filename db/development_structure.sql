38537e0a6e3f4ca711e1ab8f9879723cfae3d1b6
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2009-09-22 09:55:11 +0200

    sql dump updated

--- db/development_structure.sql
+++ db/development_structure.sql
@@ -1,120 +1,119 @@
 CREATE TABLE `audits` (
-  `id` int(11) NOT NULL auto_increment,
-  `auditable_id` int(11) default NULL,
-  `auditable_type` varchar(255) default NULL,
-  `auditable_parent_id` int(11) default NULL,
-  `auditable_parent_type` varchar(255) default NULL,
-  `user_id` int(11) default NULL,
-  `user_type` varchar(255) default NULL,
-  `username` varchar(255) default NULL,
-  `action` varchar(255) default NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `auditable_id` int(11) DEFAULT NULL,
+  `auditable_type` varchar(255) DEFAULT NULL,
+  `auditable_parent_id` int(11) DEFAULT NULL,
+  `auditable_parent_type` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `user_type` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `action` varchar(255) DEFAULT NULL,
   `changes` text,
-  `version` int(11) default '0',
-  `created_at` datetime default NULL,
-  PRIMARY KEY  (`id`),
+  `version` int(11) DEFAULT '0',
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `auditable_index` (`auditable_id`,`auditable_type`),
   KEY `auditable_parent_index` (`auditable_parent_id`,`auditable_parent_type`),
   KEY `user_index` (`user_id`,`user_type`),
   KEY `index_audits_on_created_at` (`created_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=352 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `auth_tokens` (
-  `id` int(11) NOT NULL auto_increment,
-  `domain_id` int(11) default NULL,
-  `user_id` int(11) default NULL,
-  `token` varchar(255) NOT NULL default '',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `domain_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `token` varchar(255) NOT NULL,
   `permissions` text NOT NULL,
-  `created_at` datetime default NULL,
-  `updated_at` datetime default NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   `expires_at` datetime NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `domains` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(255) default NULL,
-  `master` varchar(255) default NULL,
-  `last_check` int(11) default NULL,
-  `type` varchar(255) default 'NATIVE',
-  `notified_serial` int(11) default NULL,
-  `account` varchar(255) default NULL,
-  `ttl` int(11) default '86400',
-  `integer` int(11) default '86400',
-  `created_at` datetime default NULL,
-  `updated_at` datetime default NULL,
-  `user_id` int(11) default NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `master` varchar(255) DEFAULT NULL,
+  `last_check` int(11) DEFAULT NULL,
+  `type` varchar(255) DEFAULT 'NATIVE',
+  `notified_serial` int(11) DEFAULT NULL,
+  `account` varchar(255) DEFAULT NULL,
+  `ttl` int(11) DEFAULT '86400',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
   `notes` text,
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   KEY `index_domains_on_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `macro_steps` (
-  `id` int(11) NOT NULL auto_increment,
-  `macro_id` int(11) default NULL,
-  `action` varchar(255) default NULL,
-  `record_type` varchar(255) default NULL,
-  `name` varchar(255) default NULL,
-  `content` varchar(255) default NULL,
-  `ttl` int(11) default NULL,
-  `prio` int(11) default NULL,
-  `position` int(11) default NULL,
-  `active` tinyint(1) default '1',
-  `note` varchar(255) default NULL,
-  `created_at` datetime default NULL,
-  `updated_at` datetime default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `macro_id` int(11) DEFAULT NULL,
+  `action` varchar(255) DEFAULT NULL,
+  `record_type` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `ttl` int(11) DEFAULT NULL,
+  `prio` int(11) DEFAULT NULL,
+  `position` int(11) NOT NULL,
+  `active` tinyint(1) DEFAULT '1',
+  `note` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `macros` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(255) default NULL,
-  `description` varchar(255) default NULL,
-  `user_id` int(11) default NULL,
-  `active` tinyint(1) default '0',
-  `created_at` datetime default NULL,
-  `updated_at` datetime default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT '0',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `record_templates` (
-  `id` int(11) NOT NULL auto_increment,
-  `zone_template_id` int(11) default NULL,
-  `name` varchar(255) default NULL,
-  `record_type` varchar(255) NOT NULL default '',
-  `content` varchar(255) NOT NULL default '',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `zone_template_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `record_type` varchar(255) NOT NULL,
+  `content` varchar(255) NOT NULL,
   `ttl` int(11) NOT NULL,
-  `prio` int(11) default NULL,
-  `created_at` datetime default NULL,
-  `updated_at` datetime default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+  `prio` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `records` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `domain_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL default '',
-  `type` varchar(255) NOT NULL default '',
-  `content` varchar(255) NOT NULL default '',
+  `name` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `content` varchar(255) NOT NULL,
   `ttl` int(11) NOT NULL,
-  `prio` int(11) default NULL,
+  `prio` int(11) DEFAULT NULL,
   `change_date` int(11) NOT NULL,
-  `created_at` datetime default NULL,
-  `updated_at` datetime default NULL,
-  PRIMARY KEY  (`id`),
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `index_records_on_domain_id` (`domain_id`),
   KEY `index_records_on_name` (`name`),
   KEY `index_records_on_name_and_type` (`name`,`type`)
-) ENGINE=InnoDB AUTO_INCREMENT=233 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `roles` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(255) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `roles_users` (
-  `role_id` int(11) default NULL,
-  `user_id` int(11) default NULL,
+  `role_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
   KEY `index_roles_users_on_role_id` (`role_id`),
   KEY `index_roles_users_on_user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -125,31 +124,31 @@ CREATE TABLE `schema_migrations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL auto_increment,
-  `login` varchar(255) default NULL,
-  `email` varchar(255) default NULL,
-  `crypted_password` varchar(40) default NULL,
-  `salt` varchar(40) default NULL,
-  `created_at` datetime default NULL,
-  `updated_at` datetime default NULL,
-  `remember_token` varchar(255) default NULL,
-  `remember_token_expires_at` datetime default NULL,
-  `activation_code` varchar(40) default NULL,
-  `activated_at` datetime default NULL,
-  `state` varchar(255) default 'passive',
-  `deleted_at` datetime default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `crypted_password` varchar(40) DEFAULT NULL,
+  `salt` varchar(40) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `remember_token` varchar(255) DEFAULT NULL,
+  `remember_token_expires_at` datetime DEFAULT NULL,
+  `activation_code` varchar(40) DEFAULT NULL,
+  `activated_at` datetime DEFAULT NULL,
+  `state` varchar(255) DEFAULT 'passive',
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `zone_templates` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(255) default NULL,
-  `ttl` int(11) default '86400',
-  `created_at` datetime default NULL,
-  `updated_at` datetime default NULL,
-  `user_id` int(11) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `ttl` int(11) DEFAULT '86400',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO schema_migrations (version) VALUES ('1');
 
@@ -158,6 +157,8 @@ INSERT INTO schema_migrations (version) VALUES ('10');
 INSERT INTO schema_migrations (version) VALUES ('2');
 
 INSERT INTO schema_migrations (version) VALUES ('20081228121040');
+
+INSERT INTO schema_migrations (version) VALUES ('20090505124622');
 
 INSERT INTO schema_migrations (version) VALUES ('3');
 
