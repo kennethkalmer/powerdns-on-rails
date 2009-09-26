aac8aaef18d648796f4fd80283e687b16253eab0
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2009-09-26 22:22:23 +0200

    JSON search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,25 @@
 class SearchController < ApplicationController
-  
+
   def results
     if params[:q].chomp.blank?
-      redirect_to root_path
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.json { render :status => 404, :json => { :error => "Missing 'q' parameter" } }
+      end
     else
       @results = Domain.search(params[:q], params[:page], current_user)
-      
-      if @results.size == 1
-        redirect_to domain_path(@results.pop)
+
+      respond_to do |format|
+        format.html do
+          if @results.size == 1
+            redirect_to domain_path(@results.pop)
+          end
+        end
+        format.json do
+          render :json => @results.to_json(:only => [:id, :name])
+        end
       end
     end
   end
-  
+
 end
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,9 +1,10 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe SearchController do
+describe SearchController, "for admins" do
 
   before(:each) do
-    session[:user_id] = Factory(:admin).id
+    #session[:user_id] = Factory(:admin).id
+    login_as Factory(:admin)
 
     Factory(:domain, :name => 'example.com')
     Factory(:domain, :name => 'example.net')
@@ -39,4 +40,33 @@ describe SearchController do
     response.should redirect_to( domain_path( domain ) )
   end
 
+end
+
+describe SearchController, "for api clients" do
+  before(:each) do
+    authorize_as(Factory(:api_client))
+
+    Factory(:domain, :name => 'example.com')
+    Factory(:domain, :name => 'example.net')
+  end
+
+  it "should return an empty JSON response for no results" do
+    get :results, :q => 'amazon', :format => 'json'
+
+    assigns[:results].should be_empty
+
+    response.should have_text("[]")
+  end
+
+  it "should return a JSON set of results" do
+    get :results, :q => 'example', :format => 'json'
+
+    assigns[:results].should_not be_empty
+
+    json = ActiveSupport::JSON.decode( response.body )
+    json.size.should be(2)
+    json.first["domain"].keys.should include('id', 'name')
+    json.first["domain"]["name"].should match(/example/)
+    json.first["domain"]["id"].to_s.should match(/\d+/)
+  end
 end
