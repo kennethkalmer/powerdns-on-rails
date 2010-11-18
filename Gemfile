7bed0ad9d14c0e4bc98444c549d1a04d6a9295b6
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2010-11-18 10:26:24 +0200

    Rough upgrade to Rails 3
    Rails 3 project generated over current
    Bundled gems, removed copies from vendor/gems and vendor/plugins

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'mysql2'
+gem 'haml'
+gem 'jquery-rails'
+gem 'will_paginate', '~> 3.0.pre2'
+gem 'acts_as_audited', '2.0.0.rc2', :path => '/Users/kenneth/Code/FOSS/acts_as_audited'
+gem 'inherited_resources'
+
+group :development, :test do
+  gem "rspec-rails", "~> 2.0.0"
+end
+
+group :test do
+  gem 'ruby-debug19'
+  gem 'factory_girl_rails'
+  gem "cucumber"
+  gem 'mocha'
+  gem 'webrat'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,78 +1,44 @@
-ActionController::Routing::Routes.draw do |map|
-  # The priority is based upon order of creation: first created -> highest priority.
+PowerdnsOnRails::Application.routes.draw do
+  match '/' => 'dashboard#index'
 
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
-  #   end
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
-
-  # See how all your routes lay out with "rake routes"
-
-  map.root :controller => 'dashboard'
-
-  # RESTful zones and records
-  map.resources :domains, :has_many => :records, :member => {
-    :update_note => :put,
-    :change_owner => :put,
-    :apply_macro => [:get,:post]
-  }
-  map.resources :records, :member => { :update_soa => :put }
-  map.resources :soa, :controller => 'records'
-
-  # RESTful templates
-  map.resources :zone_templates, :controller => 'templates'
-  map.resources :record_templates
-
-  # RESTful macros
-  map.resources :macros do |macro|
-    macro.resources :macro_steps
+  resources :domains do
+    member do
+      put :change_owner
+      get :apply_macro
+      post :apply_macro
+      put :update_note
+    end
   end
 
-  # Audits
-  map.audits '/audits/:action/:id', :controller => 'audits', :action => 'index'
-  map.reports '/reports/:action/:id' , :controller => 'reports' , :action => 'index'
+  resources :records do
+    member do
+      put :update_soa
+    end
+  end
 
-  # AuthTokens
-  map.resource :auth_token
-  map.token '/token/:token', :controller => 'sessions', :action => 'token'
+  resources :soa
+  resources :zone_templates
+  resources :record_templates
 
-  # Authentication routes
-  map.resources :users, :member => { :suspend   => :put,
-                                     :unsuspend => :put,
-                                     :purge     => :delete }
-  map.resource :session
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  resources :macros do
+    resources :macro_steps
+  end
 
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing the them or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  match '/audits/:action/:id' => 'audits#index', :as => :audits
+  match '/reports/:action/:id' => 'reports#index', :as => :reports
+
+  resource :auth_token
+  match '/token/:token' => 'sessions#token', :as => :token
+
+  resources :users do
+    member do
+      put :suspend
+      put :unsuspend
+      delete :purge
+    end
+  end
+
+  resource :session
+  match '/logout' => 'sessions#destroy', :as => :logout
+  match '/:controller(/:action(/:id))'
 end
