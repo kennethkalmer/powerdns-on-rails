6862b674916938025d544c5fb0415d536d1a16dc
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2011-06-25 16:00:17 +0200

    Updated jquery-rails

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module PowerdnsOnRails
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(jquery rails application)
+    #config.action_view.javascript_expansions[:defaults] = %w(jquery rails application)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
