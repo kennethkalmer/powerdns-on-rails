9786180e7e8b5963ae4edac55011c785b06f02ea
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2011-06-26 22:36:22 +0200

    Fixed adding new records to a domain

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,21 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-// Easily control flash visibility from RJS
-// TODO: This still used ?
-function showflash( level, message ) {
-  f = $('flash-' + level);
-  f.innerHTML = message;
-  f.show();
-  setTimeout( "$('flash-" + level + "').hide()", 5000 ) // hide after 7 seconds
-}
-
-// Setup tooltips where required
 $(document).ready(function() {
+
+  // Setup tooltips where required
   $('.help-icn').each(function(i, icon){
     $(icon).tipTip({
       content: $( "#" + $(icon).data("help") ).text()
     });
+  });
+
+  // Used on /domains by the new record form
+  $('#record-form #record_type').change(function(e) {
+    toggleRecordFields( $(this).val() );
   });
 });
 
