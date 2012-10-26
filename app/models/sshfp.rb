f9b8619f9db67d8099ce63884aadbff991e560d4
Andreas Wurm <andreaswurm@gmx.de>
2012-10-26 22:16:40 +0200

    allow SHA-256 in SSHFP records

--- app/models/sshfp.rb
+++ app/models/sshfp.rb
@@ -4,8 +4,10 @@
 # 
 # Defined in RFC 4255.
 #
+# Including draft extension for SHA2 and ECDSA. See https://tools.ietf.org/html/draft-os-ietf-sshfp-ecdsa-sha2-04
+
 class SSHFP < Record
   
-  validates_format_of :content, :with => /^[1-3] [1-2] ([0-9a-f]{40})$/
-  
+  validates_format_of :content, :with => /^[1-3] [1-2] ([0-9a-fA-F]{40}|[0-9a-fA-F]{64})$/
+
 end
