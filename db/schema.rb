b6eb7f6cdbe5d8a39ac930952fa0876dd21283dc
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2013-03-13 07:43:43 +0200

    Upgraded database schema for acts_as_audited

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,25 +11,26 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110306115116) do
+ActiveRecord::Schema.define(:version => 20130313051606) do
 
   create_table "audits", :force => true do |t|
     t.integer  "auditable_id"
     t.string   "auditable_type"
-    t.integer  "association_id"
-    t.string   "association_type"
+    t.integer  "associated_id"
+    t.string   "associated_type"
     t.integer  "user_id"
     t.string   "user_type"
     t.string   "username"
     t.string   "action"
     t.text     "audited_changes"
-    t.integer  "version",          :default => 0
+    t.integer  "version",         :default => 0
     t.datetime "created_at"
     t.string   "comment"
     t.string   "remote_address"
   end
 
-  add_index "audits", ["association_id", "association_type"], :name => "auditable_parent_index"
+  add_index "audits", ["associated_id", "associated_type"], :name => "associated_index"
+  add_index "audits", ["associated_id", "associated_type"], :name => "auditable_parent_index"
   add_index "audits", ["auditable_id", "auditable_type"], :name => "auditable_index"
   add_index "audits", ["created_at"], :name => "index_audits_on_created_at"
   add_index "audits", ["user_id", "user_type"], :name => "user_index"
