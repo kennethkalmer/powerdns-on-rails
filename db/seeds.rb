18a82cb28d30b7fe982f7fa804655c20e91da5c7
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2013-03-28 21:04:22 +0200

    Seed data should respect default timezone (#37)

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ user.login = 'admin' # Not used anymore
 user.password = 'secret'
 user.password_confirmation = 'secret'
 user.admin = true
-user.confirmed_at = Time.now
+user.confirmed_at = Time.zone.now
 user.save!
 user.confirm!
 
