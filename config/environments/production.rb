5b59547ee535159d9ea4373e70198b5721e74b59
Herman verschooten <Herman@verschooten.net>
2014-03-19 23:12:34 +0100

    Use hmail in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ PowerdnsOnRails::Application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+  config.action_mailer.smtp_settings = {
+    address: "mail.hmail.be"
+  }
 end
